9daa4e514dc01d400aff7d1383806284b1b37e66
Lee Wannacott <wannacottL@gmail.com>
2021-11-30 21:03:17 +1300

    Print out deck of cards.

--- cards/main.go
+++ cards/main.go
@@ -6,10 +6,20 @@ func main() {
 	// var card string = "Ace of spades"
 	// card := "Ace of Spades"
 	// card = "Five of Diamonds"
-	card := newCard()
-	fmt.Println(card)
+	// card := newCard()
+	// cards := deck{"Ace of Spades", newCard()}
+	// cards = append(cards, "Six of Spades")
+	// cards.print()
+	// fmt.Println(cards)
+
+	cards := newDeck()
+	cards.print()
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
-func newCard() string {
-	return "Five of Diamonds"
-}
+// func newCard() string {
+// return "Five of Diamonds"
+// }
