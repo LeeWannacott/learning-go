246265b988ed8c580684e0e1272875e4d0964552
Lee Wannacott <wannacottL@gmail.com>
2021-12-01 18:06:30 +1300

    Shuffle deck of cards.

--- cards/main.go
+++ cards/main.go
@@ -17,7 +17,10 @@ func main() {
 	// cards := newDeck()
 	// cards.saveToFile("my_cards")
 	// fmt.Println(cards.toString())
-	cards := newDeckFromFile("my_cards")
+	// cards := newDeckFromFile("my_cards")
+	// cards.print()
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 	// for i, card := range cards {
 	// fmt.Println(i, card)
